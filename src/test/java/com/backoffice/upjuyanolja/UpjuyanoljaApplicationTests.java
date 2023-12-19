db5624db3c715239384fd18bd09ca2ddcbf379d0
JeongUijeong <jeong275117@gmail.com>
2023-12-19 18:19:49 +0900

    #2 feat: 프로젝트 생성

---
+++ src/test/java/com/backoffice/upjuyanolja/UpjuyanoljaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backoffice.upjuyanolja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpjuyanoljaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
