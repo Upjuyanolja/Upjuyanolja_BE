dee7eb812e1bacd915beb4e16cb2ed7b5ff548c9
JeongUijeong <jeong275117@gmail.com>
2023-12-26 14:42:09 +0900

    #3 refactor: BaseTime 엔터티 속성 코멘트 추가

--- src/main/java/com/backoffice/upjuyanolja/global/common/BaseTime.java
+++ src/main/java/com/backoffice/upjuyanolja/global/common/BaseTime.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import org.hibernate.annotations.Comment;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -15,13 +16,16 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public abstract class BaseTime {
 
     @CreatedDate
+    @Comment("생성일시")
     private LocalDateTime createdAt;
 
     @Column(insertable = false)
     @LastModifiedDate
+    @Comment("수정일시")
     private LocalDateTime updatedAt;
 
     @Column(insertable = false)
+    @Comment("삭제일시")
     private LocalDateTime deletedAt;
 
     protected void delete(LocalDateTime currentTime) {
