6f3a8dcce9b14fc1ebc759f53f3368032dd419ad
JeongUijeong <jeong275117@gmail.com>
2023-12-28 18:11:26 +0900

    #10 refactor: InvalidDataException은 RuntimeException을 상속받도록 수정
    - 공통 예외 어드바이스로 핸들링하지 않을 것이기 때문

--- src/main/java/com/backoffice/upjuyanolja/domain/openapi/exception/InvalidDataException.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/openapi/exception/InvalidDataException.java
@@ -1,11 +1,10 @@
 package com.backoffice.upjuyanolja.domain.openapi.exception;
 
-import com.backoffice.upjuyanolja.global.exception.ApplicationException;
 import com.backoffice.upjuyanolja.global.exception.ErrorCode;
 
-public class InvalidDataException extends ApplicationException {
+public class InvalidDataException extends RuntimeException {
 
     public InvalidDataException() {
-        super(ErrorCode.DATA_NOT_FOUND);
+        super(ErrorCode.DATA_NOT_FOUND.getMessage());
     }
 }
