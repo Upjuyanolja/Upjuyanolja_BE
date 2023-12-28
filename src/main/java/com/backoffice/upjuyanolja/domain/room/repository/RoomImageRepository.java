8fd25afc48f3e0c2e4b24b4685abb5e83ae93f84
JeongUijeong <jeong275117@gmail.com>
2023-12-28 17:13:05 +0900

    #10 feat: 객실 이미지 레포지토리 생성

---
+++ src/main/java/com/backoffice/upjuyanolja/domain/room/repository/RoomImageRepository.java
@@ -0,0 +1,8 @@
+package com.backoffice.upjuyanolja.domain.room.repository;
+
+import com.backoffice.upjuyanolja.domain.room.entity.RoomImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomImageRepository extends JpaRepository<RoomImage, Long> {
+
+}
