1cd8f7ffdb42cceecd9953536833723c08624aa7
cdm2883@naver.com <cdm2883@naver.com>
2023-12-28 21:43:59 +0900

    fix : 전화번호 length 12에서 13으로 수정

--- src/main/java/com/backoffice/upjuyanolja/domain/member/entity/Member.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/member/entity/Member.java
@@ -32,10 +32,10 @@ public class Member extends BaseTime {
     @Column(nullable = false, length = 30)
     @Comment("이름")
     private String name;
-    @Column(nullable = false, length = 12)
+    @Column(nullable = false, length = 13)
     @Comment("전화번호")
     private String phone;
-    @Column(nullable = false, columnDefinition = "TEXT")
+    @Column(columnDefinition = "TEXT")
     @Comment("프로필 이미지 URL")
     private String imageUrl;
     @Enumerated(EnumType.STRING)
