6083130764701adba0961d8a8e32d90394268ddb
chadongmin <cdm2883@naver.com>
2023-12-28 22:53:05 +0900

    refactor : 컨벤션에 맞게 어노테이션 수정

--- src/main/java/com/backoffice/upjuyanolja/domain/member/dto/response/SignUpResponse.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/member/dto/response/SignUpResponse.java
@@ -1,22 +1,26 @@
 package com.backoffice.upjuyanolja.domain.member.dto.response;
 
 import com.backoffice.upjuyanolja.domain.member.entity.Member;
-import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class SignUpResponse{
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SignUpResponse {
 
     private String email;
+
     private String name;
 
-    public static SignUpResponse fromEntity(Member member){
+    @Builder
+    public SignUpResponse(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+
+    public static SignUpResponse fromEntity(Member member) {
         return SignUpResponse.builder()
             .name(member.getName())
             .email(member.getEmail())
