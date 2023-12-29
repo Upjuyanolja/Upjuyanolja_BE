72e98deeab508c6c1f5e0ac87117fed57fd64210
JeongUijeong <jeong275117@gmail.com>
2023-12-29 16:47:29 +0900

    #34 fix: 클래스 오타 수정

--- src/main/java/com/backoffice/upjuyanolja/domain/member/dto/response/VerifyEmailResponse.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/member/dto/response/VerifyEmailResponse.java
@@ -3,7 +3,7 @@ package com.backoffice.upjuyanolja.domain.member.dto.response;
 import lombok.Builder;
 
 @Builder
-public record VerityEmailResponse(
+public record VerifyEmailResponse(
     boolean isExists
 ) {
 
