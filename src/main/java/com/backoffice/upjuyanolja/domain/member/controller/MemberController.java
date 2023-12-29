fad929904f8f037be52ffa859d219cf14fca0325
JeongUijeong <jeong275117@gmail.com>
2023-12-29 21:47:47 +0900

    #26 feat: 회원 정보 조회 API 구현

--- src/main/java/com/backoffice/upjuyanolja/domain/member/controller/MemberController.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/member/controller/MemberController.java
@@ -2,7 +2,9 @@ package com.backoffice.upjuyanolja.domain.member.controller;
 
 import com.backoffice.upjuyanolja.domain.member.dto.request.SignUpRequest;
 import com.backoffice.upjuyanolja.domain.member.dto.response.CheckEmailDuplicateResponse;
+import com.backoffice.upjuyanolja.domain.member.dto.response.GetMemberResponse;
 import com.backoffice.upjuyanolja.domain.member.dto.response.SignUpResponse;
+import com.backoffice.upjuyanolja.domain.member.service.MemberGetService;
 import com.backoffice.upjuyanolja.domain.member.service.MemberRegisterService;
 import com.backoffice.upjuyanolja.global.common.ApiResponse;
 import com.backoffice.upjuyanolja.global.common.ApiResponse.SuccessResponse;
@@ -11,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberRegisterService memberRegisterService;
+    private final MemberGetService memberGetService;
 
     @PostMapping("/signup")
     public ResponseEntity<SuccessResponse<SignUpResponse>> signup(
@@ -43,5 +47,15 @@ public class MemberController {
                 .message("성공적으로 이메일 중복 여부를 검사했습니다.")
                 .data(memberRegisterService.checkEmailDuplicate(email))
                 .build());
+    }
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity<SuccessResponse<GetMemberResponse>> getMember(
+        // TODO 시큐리티 로그인 적용 이후 토큰에서 memberId 받아오도록 수정
+        @PathVariable(name = "memberId") long memberId) {
+        return ApiResponse.success(HttpStatus.OK, SuccessResponse.<GetMemberResponse>builder()
+            .message("성공적으로 회원 정보를 조회했습니다.")
+            .data(memberGetService.getMember(memberId))
+            .build());
     }
 }
