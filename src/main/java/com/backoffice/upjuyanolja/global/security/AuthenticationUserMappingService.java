bc3d00228d01acaee7c8a9cdedbf5cf07dc1cb41
chadongmin <cdm2883@naver.com>
2023-12-31 11:40:56 +0900

    feat : AuthenticationUserMappingService 구현

---
+++ src/main/java/com/backoffice/upjuyanolja/global/security/AuthenticationUserMappingService.java
@@ -0,0 +1,41 @@
+package com.backoffice.upjuyanolja.global.security;
+
+import com.backoffice.upjuyanolja.domain.member.entity.Member;
+import com.backoffice.upjuyanolja.domain.member.exception.MemberNotFoundException;
+import com.backoffice.upjuyanolja.domain.member.repository.MemberRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public final class AuthenticationUserMappingService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Optional<Member> member = memberRepository.findByEmail(email);
+
+        if (member.isEmpty()) {
+            throw new MemberNotFoundException();
+        }
+
+        Member authInfo = member.get();
+
+        return User
+            .withUsername(email)
+            .password(authInfo.getPassword())//{bcrypt}로 시작
+            .authorities("USER")
+            .accountExpired(false)
+            .accountLocked(false)
+            .credentialsExpired(false)
+            .disabled(false)
+            .build();
+    }
+}
