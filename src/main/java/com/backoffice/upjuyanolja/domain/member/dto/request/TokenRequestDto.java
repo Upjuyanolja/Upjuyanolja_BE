451f2501890e5953fe1f82684f3370f01522ce97
chadongmin <cdm2883@naver.com>
2023-12-31 12:30:25 +0900

    feat : TokenRequestDto 생성

---
+++ src/main/java/com/backoffice/upjuyanolja/domain/member/dto/request/TokenRequestDto.java
@@ -0,0 +1,21 @@
+package com.backoffice.upjuyanolja.domain.member.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TokenRequestDto {
+
+    private String accessToken;
+
+    private String refreshToken;
+
+    @Builder
+    public TokenRequestDto(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+}
