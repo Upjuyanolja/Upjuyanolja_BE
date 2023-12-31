b154e89b4a41259ab01872060c86a3758712a3c5
chadongmin <cdm2883@naver.com>
2023-12-31 12:29:00 +0900

    feat : 리프레쉬 토큰 재발급 서비스 로직 구현

--- src/main/java/com/backoffice/upjuyanolja/domain/member/service/MemberAuthService.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/member/service/MemberAuthService.java
@@ -4,7 +4,9 @@ import static com.backoffice.upjuyanolja.domain.member.entity.Authority.ROLE_USE
 
 import com.backoffice.upjuyanolja.domain.member.dto.request.SignInRequest;
 import com.backoffice.upjuyanolja.domain.member.dto.request.SignUpRequest;
+import com.backoffice.upjuyanolja.domain.member.dto.request.TokenRequestDto;
 import com.backoffice.upjuyanolja.domain.member.dto.response.CheckEmailDuplicateResponse;
+import com.backoffice.upjuyanolja.domain.member.dto.response.RefreshTokenDto;
 import com.backoffice.upjuyanolja.domain.member.dto.response.SignInResponse;
 import com.backoffice.upjuyanolja.domain.member.dto.response.SignUpResponse;
 import com.backoffice.upjuyanolja.domain.member.dto.response.TokenResponseDto;
@@ -106,5 +108,39 @@ public class MemberAuthService {
 
     public boolean isDuplicatedEmail(String email) {
         return memberRepository.existsByEmail(email);
+    }
+
+    public RefreshTokenDto refresh(TokenRequestDto request) {
+
+        // 1. Refresh Token 검증
+        if (!jwtTokenProvider.validateToken(request.getRefreshToken())) {
+            throw new RuntimeException("Refresh Token 이 유효하지 않습니다.");
+        }
+
+        // 2. Access Token 에서 Member ID 가져오기
+        Authentication authentication = jwtTokenProvider.getAuthentication(request.getAccessToken());
+        log.info("멤버 아이디 :{}", authentication.getName());
+
+        // 3. 저장소에서 Member ID 를 기반으로 Refresh Token 값 가져옴
+        RefreshToken refreshToken = refreshTokenRepository.findById(authentication.getName())
+            .orElseThrow(() -> new RuntimeException("로그아웃 된 사용자입니다."));
+
+        // 4. Refresh Token 일치하는지 검사
+        if (!refreshToken.getToken().equals(request.getRefreshToken())) {
+            throw new RuntimeException("토큰의 유저 정보가 일치하지 않습니다.");
+        }
+        log.info("요청한 리프레쉬 토큰 : {} ", request.getRefreshToken());
+        log.info("디비에 저장된 리프레쉬 토큰 : {} ", refreshToken.getToken());
+
+        // 5. 새로운 토큰 생성
+        TokenResponseDto newToken = jwtTokenProvider.generateToken(authentication);
+
+        // 6. 저장소 정보 업데이트
+        RefreshToken newRefreshToken = refreshToken.updateRefreshToken(newToken.getRefreshToken());
+        refreshTokenRepository.save(newRefreshToken);
+
+        RefreshTokenDto newRefreshTokenDto = new RefreshTokenDto(newRefreshToken.getToken());
+
+        return newRefreshTokenDto;
     }
 }
