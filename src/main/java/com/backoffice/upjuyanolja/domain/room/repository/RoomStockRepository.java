00c9a522d178a9d1fdfcd94ca0092283ac53e924
JeongUijeong <jeong275117@gmail.com>
2024-01-03 21:15:45 +0900

    #39 feat: 객실 재고 Repository 생성

---
+++ src/main/java/com/backoffice/upjuyanolja/domain/room/repository/RoomStockRepository.java
@@ -0,0 +1,8 @@
+package com.backoffice.upjuyanolja.domain.room.repository;
+
+import com.backoffice.upjuyanolja.domain.room.entity.RoomStock;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomStockRepository extends JpaRepository<RoomStock, Long> {
+
+}
