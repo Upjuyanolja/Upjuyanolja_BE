be146cc8f566eab1c3293fef48ab21807d25ed9d
HyunA <vikim1210@naver.com>
2024-01-04 04:43:22 +0900

    #9 refactor: Room 분리 하면서, exception도 새로 분리

--- src/main/java/com/backoffice/upjuyanolja/domain/room/service/RoomService.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/room/service/RoomService.java
@@ -1,7 +1,7 @@
 package com.backoffice.upjuyanolja.domain.room.service;
 
-import com.backoffice.upjuyanolja.domain.accommodation.exception.AccommodationNotFoundException;
 import com.backoffice.upjuyanolja.domain.room.entity.Room;
+import com.backoffice.upjuyanolja.domain.room.exception.RoomNotFoundException;
 import com.backoffice.upjuyanolja.domain.room.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,8 +11,9 @@ import org.springframework.stereotype.Service;
 public class RoomService {
 
     private final RoomRepository roomRepository;
-    public Room findRoomById(Long roomId){
+
+    public Room findRoomById(Long roomId) {
         return roomRepository.findById(roomId)
-            .orElseThrow(() -> new AccommodationNotFoundException());
+            .orElseThrow(() -> new RoomNotFoundException());
     }
 }
