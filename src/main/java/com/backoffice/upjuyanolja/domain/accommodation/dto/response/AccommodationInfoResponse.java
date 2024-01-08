de6bc26265736fa2a0012ddf824230bd6978f9c4
JeongUijeong <jeong275117@gmail.com>
2024-01-09 04:47:12 +0900

    #54 fix: 변환 로직에서 누락된 숙소 설명, 객실 추가

--- src/main/java/com/backoffice/upjuyanolja/domain/accommodation/dto/response/AccommodationInfoResponse.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/accommodation/dto/response/AccommodationInfoResponse.java
@@ -2,6 +2,7 @@ package com.backoffice.upjuyanolja.domain.accommodation.dto.response;
 
 import com.backoffice.upjuyanolja.domain.accommodation.entity.Accommodation;
 import com.backoffice.upjuyanolja.domain.room.dto.response.RoomInfoResponse;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Builder;
 
@@ -18,15 +19,19 @@ public record AccommodationInfoResponse(
 ) {
 
     public static AccommodationInfoResponse of(Accommodation accommodation) {
+        List<RoomInfoResponse> rooms = new ArrayList<>();
+        accommodation.getRooms().forEach(room -> rooms.add(RoomInfoResponse.of(room)));
         return AccommodationInfoResponse.builder()
             .accommodationId(accommodation.getId())
             .name(accommodation.getName())
             .type(accommodation.getType().name())
+            .description(accommodation.getDescription())
             .address(accommodation.getAddress().getAddress()
                 + " "
                 + accommodation.getAddress().getDetailAddress())
             .images(AccommodationImageResponse.of(accommodation.getImages()))
             .option(AccommodationOptionResponse.of(accommodation.getOption()))
+            .rooms(rooms)
             .build();
     }
 }
