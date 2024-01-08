244795070ace8ffc29ee232a4e7bc6c79810830b
JeongUijeong <jeong275117@gmail.com>
2024-01-09 02:07:45 +0900

    #54 feat: 객실 등록 기능 구현

---
+++ src/main/java/com/backoffice/upjuyanolja/domain/room/service/RoomService.java
@@ -0,0 +1,37 @@
+package com.backoffice.upjuyanolja.domain.room.service;
+
+import com.backoffice.upjuyanolja.domain.accommodation.entity.Accommodation;
+import com.backoffice.upjuyanolja.domain.room.dto.request.RoomImageRequest;
+import com.backoffice.upjuyanolja.domain.room.dto.request.RoomRegisterRequest;
+import com.backoffice.upjuyanolja.domain.room.dto.response.RoomInfoResponse;
+import com.backoffice.upjuyanolja.domain.room.entity.Room;
+import com.backoffice.upjuyanolja.domain.room.exception.DuplicateRoomNameException;
+import com.backoffice.upjuyanolja.domain.room.repository.RoomImageRepository;
+import com.backoffice.upjuyanolja.domain.room.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RoomService {
+
+    private final RoomRepository roomRepository;
+    private final RoomImageRepository roomImageRepository;
+
+    public RoomInfoResponse saveRoom(Accommodation accommodation, RoomRegisterRequest request) {
+        roomNameValidate(request.name());
+        Room room = roomRepository.save(RoomRegisterRequest.toEntity(accommodation, request));
+        roomImageRepository.saveAll(RoomImageRequest.toEntity(room, request.images()));
+        return RoomInfoResponse.of(room);
+    }
+
+    private void roomNameValidate(String name) {
+        if (isDuplicatedRoomName(name)) {
+            throw new DuplicateRoomNameException();
+        }
+    }
+
+    public boolean isDuplicatedRoomName(String name) {
+        return roomRepository.existsRoomByName(name);
+    }
+}
