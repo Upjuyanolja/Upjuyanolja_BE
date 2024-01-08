969de1fafe1d2786c896b38dd78d3994967f7c9a
JeongUijeong <jeong275117@gmail.com>
2024-01-09 02:15:25 +0900

    #54 feat: 숙소 등록 기능 구현

---
+++ src/main/java/com/backoffice/upjuyanolja/domain/accommodation/service/AccommodationService.java
@@ -0,0 +1,62 @@
+package com.backoffice.upjuyanolja.domain.accommodation.service;
+
+import com.backoffice.upjuyanolja.domain.accommodation.dto.request.AccommodationImageRequest;
+import com.backoffice.upjuyanolja.domain.accommodation.dto.request.AccommodationRegisterRequest;
+import com.backoffice.upjuyanolja.domain.accommodation.dto.response.AccommodationInfoResponse;
+import com.backoffice.upjuyanolja.domain.accommodation.entity.Accommodation;
+import com.backoffice.upjuyanolja.domain.accommodation.entity.AccommodationImage;
+import com.backoffice.upjuyanolja.domain.accommodation.entity.AccommodationOwnership;
+import com.backoffice.upjuyanolja.domain.accommodation.repository.AccommodationImageRepository;
+import com.backoffice.upjuyanolja.domain.accommodation.repository.AccommodationOwnershipRepository;
+import com.backoffice.upjuyanolja.domain.accommodation.repository.AccommodationRepository;
+import com.backoffice.upjuyanolja.domain.member.entity.Member;
+import com.backoffice.upjuyanolja.domain.member.service.MemberGetService;
+import com.backoffice.upjuyanolja.domain.room.dto.request.RoomRegisterRequest;
+import com.backoffice.upjuyanolja.domain.room.service.RoomService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccommodationService {
+
+    private final AccommodationRepository accommodationRepository;
+    private final AccommodationImageRepository accommodationImageRepository;
+    private final AccommodationOwnershipRepository accommodationOwnershipRepository;
+    private final RoomService roomService;
+    private final MemberGetService memberGetService;
+
+    public AccommodationInfoResponse createAccommodation(
+        long memberId,
+        AccommodationRegisterRequest request
+    ) {
+        Member member = memberGetService.getMemberById(memberId);
+        Accommodation accommodation = saveAccommodation(request);
+        saveOwnership(member, accommodation);
+        saveRooms(accommodation, request.rooms());
+
+        return AccommodationInfoResponse.of(accommodation);
+    }
+
+    private Accommodation saveAccommodation(AccommodationRegisterRequest request) {
+        Accommodation accommodation = accommodationRepository
+            .save(AccommodationRegisterRequest.toEntity(request));
+        List<AccommodationImage> images = AccommodationImageRequest
+            .toEntity(accommodation, request.images());
+        accommodationImageRepository.saveAll(images);
+
+        return accommodation;
+    }
+
+    private void saveOwnership(Member member, Accommodation accommodation) {
+        accommodationOwnershipRepository.save(AccommodationOwnership.builder()
+            .accommodation(accommodation)
+            .member(member)
+            .build());
+    }
+
+    private void saveRooms(Accommodation accommodation, List<RoomRegisterRequest> requests) {
+        requests.forEach(request -> roomService.saveRoom(accommodation, request));
+    }
+}
