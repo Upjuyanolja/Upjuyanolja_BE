829f26fa6c096a3cf40d2c8a737a88db5d2c320b
JeongUijeong <jeong275117@gmail.com>
2024-01-10 11:33:17 +0900

    #54 refactor: 트랜잭션 추가

--- src/main/java/com/backoffice/upjuyanolja/domain/accommodation/service/AccommodationService.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/accommodation/service/AccommodationService.java
@@ -17,8 +17,10 @@ import com.backoffice.upjuyanolja.domain.room.service.RoomService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class AccommodationService {
 
--- src/main/java/com/backoffice/upjuyanolja/domain/room/service/RoomService.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/room/service/RoomService.java
@@ -11,8 +11,10 @@ import com.backoffice.upjuyanolja.domain.room.repository.RoomImageRepository;
 import com.backoffice.upjuyanolja.domain.room.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class RoomService {
 
