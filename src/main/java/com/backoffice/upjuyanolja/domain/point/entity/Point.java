e3bc3e8794dbe725b6b236dbd9723b540b03099d
HyunA <vikim1210@naver.com>
2024-01-11 11:53:55 +0900

    #52 refactor: 단방향으로 포인트 엔터티 수정

--- src/main/java/com/backoffice/upjuyanolja/domain/point/entity/Point.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/point/entity/Point.java
@@ -2,7 +2,6 @@ package com.backoffice.upjuyanolja.domain.point.entity;
 
 import com.backoffice.upjuyanolja.domain.member.entity.Member;
 import com.backoffice.upjuyanolja.global.common.BaseTime;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -13,9 +12,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -46,32 +42,18 @@ public class Point extends BaseTime {
     @Comment("회원 식별자")
     private Member member;
 
-    @OneToMany(mappedBy = "point", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<PointUsage> usages = new ArrayList<>();
-
-    @OneToMany(mappedBy = "point", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<PointCharges> charges = new ArrayList<>();
-
-    @OneToMany(mappedBy = "point", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<PointRefunds> refunds = new ArrayList<>();
 
     @Builder
     public Point(
         Long id,
         int pointBalance,
         PointType pointType,
-        Member member,
-        List<PointUsage> usages,
-        List<PointCharges> charges,
-        List<PointRefunds> refunds
+        Member member
     ) {
         this.id = id;
         this.pointBalance = pointBalance;
         this.pointType = pointType;
         this.member = member;
-        this.usages = usages;
-        this.charges = charges;
-        this.refunds = refunds;
     }
 
 }
