185f8fede06727c0671ce80d21681118dc19bfac
JeongUijeong <jeong275117@gmail.com>
2024-01-12 00:02:58 +0900

    #66 feat: 회원으로 숙소 소유권 조회 메서드 추가

--- src/main/java/com/backoffice/upjuyanolja/domain/accommodation/repository/AccommodationOwnershipRepository.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/accommodation/repository/AccommodationOwnershipRepository.java
@@ -1,9 +1,12 @@
 package com.backoffice.upjuyanolja.domain.accommodation.repository;
 
 import com.backoffice.upjuyanolja.domain.accommodation.entity.AccommodationOwnership;
+import com.backoffice.upjuyanolja.domain.member.entity.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccommodationOwnershipRepository extends
     JpaRepository<AccommodationOwnership, Long> {
 
+    List<AccommodationOwnership> findAllByMember(Member member);
 }
