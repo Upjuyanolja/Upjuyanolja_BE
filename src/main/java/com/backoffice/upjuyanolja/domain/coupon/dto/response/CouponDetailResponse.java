19981b222711347564d9c88cd24ecec95789d8ce
HyunA <vikim1210@naver.com>
2024-01-12 04:32:00 +0900

    #24 perf: 상세 조회의 '룸에 해당 하는 쿠폰' 응답 dto 완성

--- src/main/java/com/backoffice/upjuyanolja/domain/coupon/dto/response/CouponDetailResponse.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/coupon/dto/response/CouponDetailResponse.java
@@ -2,26 +2,23 @@ package com.backoffice.upjuyanolja.domain.coupon.dto.response;
 
 import com.backoffice.upjuyanolja.domain.coupon.entity.Coupon;
 import com.backoffice.upjuyanolja.domain.coupon.entity.DiscountType;
-import java.time.format.DateTimeFormatter;
 import lombok.Builder;
 
 @Builder
 public record CouponDetailResponse(
     Long id,
-    String couponName,
-    String endDate,
-    String status,
-    int count
-
+    String name,
+    int price
 ) {
 
-    public static CouponDetailResponse of(Coupon coupon) {
+    public static CouponDetailResponse of(Coupon coupon, int discount) {
         return CouponDetailResponse.builder()
             .id(coupon.getId())
-            .couponName(DiscountType.makeTitleName(coupon.getDiscountType(), coupon.getDiscount()))
-            .endDate(coupon.getEndDate().format(DateTimeFormatter.ISO_LOCAL_DATE))
-            .status(coupon.getCouponStatus().getDescription())
-            .count(coupon.getCount())
+            .name(DiscountType.makeName(
+                coupon.getDiscountType(), coupon.getDiscount(),
+                coupon.getDiscountType().getDetailName()
+            ))
+            .price(discount)
             .build();
     }
 
