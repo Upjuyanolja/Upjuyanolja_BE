36224fdf3975c6c77facb023042d7f84319c70da
HyunA <vikim1210@naver.com>
2024-01-12 16:53:00 +0900

    #52 feat&refactor: 멘토링 반영한 포인트 연관 관계 수정 및 다른 도메인 개발 위한 틀

---
+++ src/main/java/com/backoffice/upjuyanolja/domain/point/controller/PointController.java
@@ -0,0 +1,12 @@
+package com.backoffice.upjuyanolja.domain.point.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/points")
+@RequiredArgsConstructor
+public class PointController {
+
+}
