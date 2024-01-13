fbb6171c036538f0bc475c91bc1a35618b371008
JeongUijeong <jeong275117@gmail.com>
2024-01-13 20:39:39 +0900

    #79 feat: 객실 수정 기능 구현

--- src/main/java/com/backoffice/upjuyanolja/domain/room/service/usecase/RoomCommandUseCase.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/room/service/usecase/RoomCommandUseCase.java
@@ -2,6 +2,7 @@ package com.backoffice.upjuyanolja.domain.room.service.usecase;
 
 import com.backoffice.upjuyanolja.domain.accommodation.entity.Accommodation;
 import com.backoffice.upjuyanolja.domain.room.dto.request.RoomRegisterRequest;
+import com.backoffice.upjuyanolja.domain.room.dto.request.RoomUpdateRequest;
 import com.backoffice.upjuyanolja.domain.room.dto.response.RoomInfoResponse;
 
 public interface RoomCommandUseCase {
@@ -9,4 +10,6 @@ public interface RoomCommandUseCase {
     RoomInfoResponse registerRoom(long memberId, long accommodationId, RoomRegisterRequest request);
 
     RoomInfoResponse saveRoom(Accommodation accommodation, RoomRegisterRequest request);
+
+    RoomInfoResponse modifyRoom(long memberId, long roomId, RoomUpdateRequest request);
 }
