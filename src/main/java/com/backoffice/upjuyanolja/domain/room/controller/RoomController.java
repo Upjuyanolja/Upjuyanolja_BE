8b3d886489e0f3d73971e8f24674134188ac5b0b
JeongUijeong <jeong275117@gmail.com>
2024-01-15 18:06:17 +0900

    #68 feat: 객실 목록 조회, 객실 상세 조회 API 구현

--- src/main/java/com/backoffice/upjuyanolja/domain/room/controller/RoomController.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/room/controller/RoomController.java
@@ -1,8 +1,10 @@
 package com.backoffice.upjuyanolja.domain.room.controller;
 
+import com.backoffice.upjuyanolja.domain.room.dto.request.RoomPageRequest;
 import com.backoffice.upjuyanolja.domain.room.dto.request.RoomRegisterRequest;
 import com.backoffice.upjuyanolja.domain.room.dto.request.RoomUpdateRequest;
 import com.backoffice.upjuyanolja.domain.room.dto.response.RoomInfoResponse;
+import com.backoffice.upjuyanolja.domain.room.dto.response.RoomPageResponse;
 import com.backoffice.upjuyanolja.domain.room.service.usecase.RoomCommandUseCase;
 import com.backoffice.upjuyanolja.global.common.response.ApiResponse;
 import com.backoffice.upjuyanolja.global.common.response.ApiResponse.SuccessResponse;
@@ -11,11 +13,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -42,6 +46,42 @@ public class RoomController {
                     accommodationId,
                     roomRegisterRequest)
                 )
+                .build()
+        );
+    }
+
+    @GetMapping("/list/{accommodationId}")
+    public ResponseEntity<SuccessResponse<RoomPageResponse>> getRooms(
+        @PathVariable long accommodationId,
+        @RequestParam(defaultValue = "0") int pageNum,
+        @RequestParam(defaultValue = "10") int pageSize
+    ) {
+        log.info("GET /api/rooms/list/{}", accommodationId);
+
+        return ApiResponse.success(HttpStatus.OK,
+            SuccessResponse.<RoomPageResponse>builder()
+                .message("성공적으로 객실 목록을 조회했습니다.")
+                .data(roomCommandUseCase.getRooms(
+                        securityUtil.getCurrentMemberId(),
+                        accommodationId,
+                        RoomPageRequest.builder()
+                            .pageNum(pageNum)
+                            .pageSize(pageSize)
+                            .build().of()
+                    )
+                )
+                .build()
+        );
+    }
+
+    @GetMapping("/{roomId}")
+    public ResponseEntity<SuccessResponse<RoomInfoResponse>> getRoom(@PathVariable long roomId) {
+        log.info("GET /api/rooms/{}", roomId);
+
+        return ApiResponse.success(HttpStatus.OK,
+            SuccessResponse.<RoomInfoResponse>builder()
+                .message("성공적으로 객실을 조회했습니다.")
+                .data(roomCommandUseCase.getRoom(securityUtil.getCurrentMemberId(), roomId))
                 .build()
         );
     }
