d46f2deaffb4519e901e68732690eff7f22f81b7
HyunA <vikim1210@naver.com>
2024-01-15 21:28:55 +0900

    #51 feat: 멤버 엔터티 통합 완성

--- src/main/java/com/backoffice/upjuyanolja/domain/member/entity/Member.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/member/entity/Member.java
@@ -1,22 +1,13 @@
 package com.backoffice.upjuyanolja.domain.member.entity;
 
-import static com.backoffice.upjuyanolja.domain.member.entity.Authority.ROLE_USER;
-
 import com.backoffice.upjuyanolja.global.common.entity.BaseTime;
-import com.backoffice.upjuyanolja.domain.point.entity.Point;
-import com.backoffice.upjuyanolja.domain.room.entity.Room;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
