709adab6e8347505d8a2abce5f290113d7f4778a
chadongmin <cdm2883@naver.com>
2024-01-16 21:59:55 +0900

    hotfix : 가상 입점 DB에 필요없는 필드 삭제

--- src/main/java/com/backoffice/upjuyanolja/domain/member/entity/Owner.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/member/entity/Owner.java
@@ -1,11 +1,8 @@
 package com.backoffice.upjuyanolja.domain.member.entity;
 
-import static com.backoffice.upjuyanolja.domain.member.entity.Authority.ROLE_ADMIN;
 import com.backoffice.upjuyanolja.global.common.entity.BaseTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,10 +26,6 @@ public class Owner extends BaseTime {
     @Comment("이메일")
     private String email;
 
-    @Column(nullable = false)
-    @Comment("암호화된 비밀번호")
-    private String password;
-
     @Column(nullable = false, length = 30)
     @Comment("이름")
     private String name;
@@ -41,29 +34,15 @@ public class Owner extends BaseTime {
     @Comment("전화번호")
     private String phone;
 
-    @Column(columnDefinition = "TEXT")
-    @Comment("프로필 이미지 URL")
-    private String imageUrl;
-
-    @Enumerated(EnumType.STRING)
-    @Comment("권한")
-    private Authority authority = ROLE_ADMIN;
-
     @Builder
     public Owner(Long id,
         String email,
-        String password,
         String name,
-        String phone,
-        String imageUrl,
-        Authority authority
+        String phone
     ) {
         this.id = id;
         this.email = email;
-        this.password = password;
         this.name = name;
         this.phone = phone;
-        this.imageUrl = imageUrl;
-        this.authority = authority;
     }
 }
