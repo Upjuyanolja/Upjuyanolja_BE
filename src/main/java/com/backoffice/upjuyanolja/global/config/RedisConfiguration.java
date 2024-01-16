2724b86986680d685bd9b33094929c15c7361317
chaewon12 <djacltk@naver.com>
2024-01-16 18:07:18 +0900

    #75 fix: RedissonClient 활성화 Profile 지정

--- src/main/java/com/backoffice/upjuyanolja/global/config/RedisConfiguration.java
+++ src/main/java/com/backoffice/upjuyanolja/global/config/RedisConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -56,6 +57,7 @@ public class RedisConfiguration {
     return redisTemplate;
   }
 
+  @Profile("prod")
   @Bean
   public RedissonClient redissonClient() {
     Config config = new Config();
