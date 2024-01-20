a4ed5c4f561a9ac25c2ffbd26a720e285321a5e6
chadongmin <cdm2883@naver.com>
2024-01-20 12:56:19 +0900

    refactor : OwnerSignupRequest 에서 name, phone, imageUrl 필드 삭제

--- src/main/java/com/backoffice/upjuyanolja/domain/member/dto/request/OwnerSignupRequest.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/member/dto/request/OwnerSignupRequest.java
@@ -18,28 +18,14 @@ public class OwnerSignupRequest {
     @NotBlank
     private String email;
 
-    @NotBlank(message = "이름을 입력해주세요")
-    @Size(min = 2, max = 12, message = "올바른 이름을 입력해주세요")
-    private String name;
-
     @NotBlank(message = "비밀번호를 입력해주세요")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,20}$", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
 
-    @NotBlank
-    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "휴대폰 번호 양식에 맞지 않습니다.")
-    private String phone;
-
-    @Nullable
-    private String imageUrl;
 
     @Builder
-    public OwnerSignupRequest(String email, String name, String password, String phone,
-        @Nullable String imageUrl) {
+    public OwnerSignupRequest(String email, String password) {
         this.email = email;
-        this.name = name;
         this.password = password;
-        this.phone = phone;
-        this.imageUrl = imageUrl;
     }
 }
