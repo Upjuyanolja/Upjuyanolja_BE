8c8e1630ac155758e195cbb4d07702f40d824659
chadongmin <cdm2883@naver.com>
2024-01-22 13:12:37 +0900

    refactor : 회원가입시 휴대폰번호 정규식 수정 ('-'제외)

--- src/main/java/com/backoffice/upjuyanolja/domain/member/dto/request/SignUpRequest.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/member/dto/request/SignUpRequest.java
@@ -22,7 +22,7 @@ public record SignUpRequest(
     String password,
 
     @NotBlank
-    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "휴대폰 번호 양식에 맞지 않습니다.")
+    @Pattern(regexp = "^[0-9]{11}$", message = "휴대폰 번호 양식에 맞지 않습니다.")
     String phone,
 
     @Nullable
