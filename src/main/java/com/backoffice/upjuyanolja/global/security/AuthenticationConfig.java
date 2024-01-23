72d9a5013b84461f03228e70c8ab21eed4c66491
JeongUijeong <jeong275117@gmail.com>
2024-01-24 00:33:36 +0900

    refactor: 권한 설정 수정

--- src/main/java/com/backoffice/upjuyanolja/global/security/AuthenticationConfig.java
+++ src/main/java/com/backoffice/upjuyanolja/global/security/AuthenticationConfig.java
@@ -33,8 +33,8 @@ public class AuthenticationConfig {
         "/api/open-api"
     };
     private static final String[] PERMIT_OWNER_URL_ARRAY = {
-        "/api/accommodations/backoffice",
-        "/api/coupons/backoffice",
+        "/api/accommodations/backoffice/**",
+        "/api/coupons/backoffice/**",
         "/api/points/**",
         "/api/rooms/**"
     };
