ca73cf9749a008aec3dd74eca9d5f478a430f46f
Jisang Lee <matrixpower1004@gmail.com>
2024-01-23 21:26:45 +0900

    refactor: CouponProperties 삭제
    - 중간 피드백 전달에서 지적된 CouponProperties를 삭제하고 쿠폰 할인 validation 설정값을 예전대로 Enum에 static final로 선언함.
    - 파일 삭제에 따른 static filed import 수정.
    - 기타 공백 수정 및 빈 import 삭제

---
+++ src/main/java/com/backoffice/upjuyanolja/domain/coupon/entity/DiscountRestrictions.java
@@ -0,0 +1,40 @@
+package com.backoffice.upjuyanolja.domain.coupon.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum DiscountRestrictions {
+
+    MIN_WON_DISCOUNT(1000),
+    MAX_WON_DISCOUNT(50000),
+
+    MIN_DISCOUNT_RATE(1),
+    MAX_DISCOUNT_RATE(50),
+    DAY_LIMIT(-1);
+
+    private final int limit;
+
+    DiscountRestrictions(int limit) {
+        this.limit = limit;
+    }
+
+    public static int getMinPrice() {
+        return MIN_WON_DISCOUNT.limit;
+    }
+
+    public static int getMaxPrice() {
+        return MAX_WON_DISCOUNT.limit;
+    }
+
+    public static int getMinRate() {
+        return MIN_DISCOUNT_RATE.limit;
+    }
+
+    public static int getMaxRate() {
+        return MAX_DISCOUNT_RATE.limit;
+    }
+
+    public static int getDayLimit() {
+        return DAY_LIMIT.limit;
+    }
+}
