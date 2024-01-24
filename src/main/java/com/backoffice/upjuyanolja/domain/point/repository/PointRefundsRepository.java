75c0fb6c2cbfef1ddbf0c353d02359a34ef2f905
HyunA <vikim1210@naver.com>
2024-01-25 07:43:17 +0900

    #108 feat: PointCharge 사용한 조회 메서드

--- src/main/java/com/backoffice/upjuyanolja/domain/point/repository/PointRefundsRepository.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/point/repository/PointRefundsRepository.java
@@ -1,8 +1,11 @@
 package com.backoffice.upjuyanolja.domain.point.repository;
 
+import com.backoffice.upjuyanolja.domain.point.entity.PointCharges;
 import com.backoffice.upjuyanolja.domain.point.entity.PointRefunds;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PointRefundsRepository extends JpaRepository<PointRefunds, Long> {
 
+    Optional<PointRefunds> findByPointCharges(PointCharges pointCharges);
 }
--- src/main/java/com/backoffice/upjuyanolja/domain/point/repository/PointUsageRepository.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/point/repository/PointUsageRepository.java
@@ -1,12 +1,12 @@
 package com.backoffice.upjuyanolja.domain.point.repository;
 
-import com.backoffice.upjuyanolja.domain.point.entity.Point;
+import com.backoffice.upjuyanolja.domain.point.entity.PointCharges;
 import com.backoffice.upjuyanolja.domain.point.entity.PointUsage;
-import java.time.YearMonth;
-import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-public interface PointUsageRepository extends JpaRepository<PointUsage, Long>, PointUsageCustomRepository {
+public interface PointUsageRepository extends JpaRepository<PointUsage, Long>,
+    PointUsageCustomRepository {
 
+    Optional<PointUsage> findByPointCharges(PointCharges pointCharges);
 }
