5bb0f6b0e70f88dcd4817dfaf6f03c06e30b6a76
HyunA <vikim1210@naver.com>
2024-01-26 21:49:49 +0900

    #108 feat: 사용 포인트와 발급 쿠폰 내역을 관리할 PointCouponIssuance 테이블 새로 생성

---
+++ src/main/java/com/backoffice/upjuyanolja/domain/point/repository/PointCouponIssuanceRepository.java
@@ -0,0 +1,8 @@
+package com.backoffice.upjuyanolja.domain.point.repository;
+
+import com.backoffice.upjuyanolja.domain.point.entity.PointCouponIssuance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PointCouponIssuanceRepository extends JpaRepository<PointCouponIssuance, Long> {
+
+}
