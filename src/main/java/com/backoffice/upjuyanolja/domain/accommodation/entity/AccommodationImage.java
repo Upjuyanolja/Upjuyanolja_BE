a4ba61adcf15e1b3bb9318c3c32672ef451cf538
JeongUijeong <jeong275117@gmail.com>
2024-02-12 19:56:26 +0900

    #228 refactor: 숙소 관련 Entity javadoc 작성 및 리팩토링

--- src/main/java/com/backoffice/upjuyanolja/domain/accommodation/entity/AccommodationImage.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/accommodation/entity/AccommodationImage.java
@@ -14,27 +14,49 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
 
+/**
+ * 숙소 이미지 Entity Class
+ *
+ * @author JeongUijeong (jeong275117@gmail.com)
+ */
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class AccommodationImage {
 
+    /**
+     * 숙소 이미지 식별자
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Comment("숙소 이미지 식별자")
     private Long id;
 
+    /**
+     * 숙소 Entity
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false, name = "accommodation_id")
     @Comment("숙소 식별자")
     private Accommodation accommodation;
 
+    /**
+     * 숙소 이미지 URL
+     */
     @Column(nullable = false, columnDefinition = "TEXT")
-    @Comment("이미지 URL")
+    @Comment("숙소 이미지 URL")
     private String url;
 
+    /**
+     * 숙소 이미지 Entity Builder
+     *
+     * @param id            숙소 이미지 식별자
+     * @param accommodation 숙소 Entity
+     * @param url           숙소 이미지 URL
+     * @author JeongUijeong (jeong275117@gmail.com)
+     */
     @Builder
-    public AccommodationImage(
+    private AccommodationImage(
         Long id,
         Accommodation accommodation,
         String url
--- src/main/java/com/backoffice/upjuyanolja/domain/accommodation/entity/AccommodationOption.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/accommodation/entity/AccommodationOption.java
@@ -14,58 +14,112 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
 
+/**
+ * 숙소 옵션 Entity Class
+ *
+ * @author JeongUijeong (jeong275117@gmail.com)
+ */
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class AccommodationOption {
 
+    /**
+     * 숙소 옵션 식별자
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Comment("숙소 옵션 식별자")
     private Long id;
 
+    /**
+     * 숙소 Entity
+     */
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     @Comment("숙소 식별자")
     private Accommodation accommodation;
 
+    /**
+     * 숙소 객실 내 취사 여부
+     */
     @Column(nullable = false)
     @Comment("객실 내 취사 여부")
     private boolean cooking;
 
+    /**
+     * 숙소 주차 시설 여부
+     */
     @Column(nullable = false)
     @Comment("주차 시설 여부")
     private boolean parking;
 
+    /**
+     * 숙소 픽업 서비스 여부
+     */
     @Column(nullable = false)
     @Comment("픽업 서비스 여부")
     private boolean pickup;
 
+    /**
+     * 숙소 바베큐 장 여부
+     */
     @Column(nullable = false)
-    @Comment("바비큐 장 여부")
+    @Comment("바베큐 장 여부")
     private boolean barbecue;
 
+    /**
+     * 숙소 피트니스 센터 여부
+     */
     @Column(nullable = false)
     @Comment("피트니스 센터 여부")
     private boolean fitness;
 
+    /**
+     * 숙소 노래방 여부
+     */
     @Column(nullable = false)
     @Comment("노래방 여부")
     private boolean karaoke;
 
+    /**
+     * 숙소 사우나 여부
+     */
     @Column(nullable = false)
     @Comment("사우나 여부")
     private boolean sauna;
 
+    /**
+     * 숙소 스포츠 시설 여부
+     */
     @Column(nullable = false)
     @Comment("스포츠 시설 여부")
     private boolean sports;
 
+    /**
+     * 숙소 세미나실 여부
+     */
     @Column(nullable = false)
-    @Comment("세미나 실 여부")
+    @Comment("세미나실 여부")
     private boolean seminar;
 
+    /**
+     * 숙소 옵션 Entity Builder
+     *
+     * @param id            숙소 옵션 식별자
+     * @param accommodation 숙소 Entity
+     * @param cooking       숙소 객실 내 취사 여부
+     * @param parking       숙소 주차 시설 여부
+     * @param pickup        숙소 픽업 서비스 여부
+     * @param barbecue      숙소 바베큐 장 여부
+     * @param fitness       숙소 피트니스 센터 여부
+     * @param karaoke       숙소 노래방 여부
+     * @param sauna         숙소 사우나 여부
+     * @param sports        숙소 스포츠 시설 여부
+     * @param seminar       숙소 세미나실 여부
+     * @author JeongUijeong (jeong275117@gmail.com)
+     */
     @Builder
-    public AccommodationOption(
+    private AccommodationOption(
         Long id,
         Accommodation accommodation,
         boolean cooking,
