cbee0eac3557e23b9ee9329cdd188493bbcc0651
JeongUijeong <jeong275117@gmail.com>
2024-02-12 19:46:31 +0900

    #228 refactor: 숙소 기능 관련 Exception javadoc 작성

--- src/main/java/com/backoffice/upjuyanolja/domain/accommodation/exception/AccommodationNotFoundException.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/accommodation/exception/AccommodationNotFoundException.java
@@ -3,8 +3,18 @@ package com.backoffice.upjuyanolja.domain.accommodation.exception;
 import com.backoffice.upjuyanolja.global.exception.ApplicationException;
 import com.backoffice.upjuyanolja.global.exception.ErrorCode;
 
+/**
+ * 숙소를 찾지 못한 경우 던질 Exception Class
+ *
+ * @author JeongUijeong (jeong275117@gmail.com)
+ */
 public class AccommodationNotFoundException extends ApplicationException {
 
+    /**
+     * No Arguments Constructor
+     *
+     * @author JeongUijeong (jeong275117@gmail.com)
+     */
     public AccommodationNotFoundException() {
         super(ErrorCode.ACCOMMODATION_NOT_FOUND);
     }
