f3d6b57fe9c9f641610e0bde14adfed7229f8372
JeongUijeong <jeong275117@gmail.com>
2024-02-12 22:58:37 +0900

    #228 refactor: 객실 관련 Exception javadoc 작성

--- src/main/java/com/backoffice/upjuyanolja/domain/room/exception/DuplicateRoomNameException.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/room/exception/DuplicateRoomNameException.java
@@ -3,8 +3,18 @@ package com.backoffice.upjuyanolja.domain.room.exception;
 import com.backoffice.upjuyanolja.global.exception.ApplicationException;
 import com.backoffice.upjuyanolja.global.exception.ErrorCode;
 
+/**
+ * 중복된 객실 이름으로 객실 등록/수정을 요청한 경우 던질 Exception Class
+ *
+ * @author JeongUijeong (jeong275117@gmail.com)
+ */
 public class DuplicateRoomNameException extends ApplicationException {
 
+    /**
+     * No Arguments Constructor
+     *
+     * @author JeongUijeong (jeong275117@gmail.com)
+     */
     public DuplicateRoomNameException() {
         super(ErrorCode.DUPLICATED_ROOM_NAME);
     }
--- src/main/java/com/backoffice/upjuyanolja/domain/room/exception/RoomNotFoundException.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/room/exception/RoomNotFoundException.java
@@ -3,8 +3,18 @@ package com.backoffice.upjuyanolja.domain.room.exception;
 import com.backoffice.upjuyanolja.global.exception.ApplicationException;
 import com.backoffice.upjuyanolja.global.exception.ErrorCode;
 
+/**
+ * 객실을 찾지 못한 경우 던질 Exception Class
+ *
+ * @author JeongUijeong (jeong275117@gmail.com)
+ */
 public class RoomNotFoundException extends ApplicationException {
 
+    /**
+     * No Arguments Constructor
+     *
+     * @author JeongUijeong (jeong275117@gmail.com)
+     */
     public RoomNotFoundException() {
         super(ErrorCode.ROOM_NOT_FOUND);
     }
