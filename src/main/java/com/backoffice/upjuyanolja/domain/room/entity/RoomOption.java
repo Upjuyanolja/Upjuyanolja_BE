c7af026c567a93b31aac5afd89bf7aa265b65688
JeongUijeong <jeong275117@gmail.com>
2024-02-12 22:46:31 +0900

    #228 refactor: 객실 관련 Entity javadoc 작성 및 리팩토링

--- src/main/java/com/backoffice/upjuyanolja/domain/room/entity/RoomOption.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/room/entity/RoomOption.java
@@ -1,7 +1,6 @@
 package com.backoffice.upjuyanolja.domain.room.entity;
 
-import com.backoffice.upjuyanolja.domain.room.service.usecase.RoomCommandUseCase.RoomOptionUpdate;
-import com.backoffice.upjuyanolja.global.common.entity.BaseTime;
+import com.backoffice.upjuyanolja.domain.room.service.usecase.RoomCommandUseCase.RoomOptionUpdateDto;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,41 +9,70 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
-import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
 
+/**
+ * 객실 옵션 Entity Class
+ *
+ * @author JeongUijeong (jeong275117@gmail.com)
+ */
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class RoomOption extends BaseTime {
+public class RoomOption {
 
+    /**
+     * 객실 옵션 식별자
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Comment("객실 옵션 식별자")
     private Long id;
 
+    /**
+     * 객실 Entity
+     */
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     @Comment("객실 식별자")
     private Room room;
 
+    /**
+     * 객실 에어컨 여부
+     */
     @Column(nullable = false)
     @Comment("에어컨 여부")
     private boolean airCondition;
 
+    /**
+     * 객실 TV 여부
+     */
     @Column(nullable = false)
     @Comment("TV 여부")
     private boolean tv;
 
+    /**
+     * 객실 인터넷 여부
+     */
     @Column(nullable = false)
     @Comment("인터넷 여부")
     private boolean internet;
 
+    /**
+     * 객실 옵션 Entity Builder
+     *
+     * @param id           객실 옵션 식별자
+     * @param room         객실 Entity
+     * @param airCondition 객실 에어컨 여부
+     * @param tv           객실 TV 여부
+     * @param internet     객실 인터넷 여부
+     * @author JeongUijeong (jeong275117@gmail.com)
+     */
     @Builder
-    public RoomOption(
+    private RoomOption(
         Long id,
         Room room,
         boolean airCondition,
@@ -58,15 +86,15 @@ public class RoomOption extends BaseTime {
         this.internet = internet;
     }
 
-    public void updateRoomOption(RoomOptionUpdate request) {
-        this.airCondition = request.airCondition();
-        this.tv = request.tv();
-        this.internet = request.internet();
+    /**
+     * 객실 옵션 수정
+     *
+     * @param roomOptionUpdateDto 객실 옵션 수정 DTO
+     * @author JeongUijeong (jeong275117@gmail.com)
+     */
+    public void update(RoomOptionUpdateDto roomOptionUpdateDto) {
+        this.airCondition = roomOptionUpdateDto.airCondition();
+        this.tv = roomOptionUpdateDto.tv();
+        this.internet = roomOptionUpdateDto.internet();
     }
-
-    @Override
-    public void delete(LocalDateTime currentTime) {
-        super.delete(currentTime);
-    }
-
 }
--- src/main/java/com/backoffice/upjuyanolja/domain/room/entity/RoomStatus.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/room/entity/RoomStatus.java
@@ -2,14 +2,35 @@ package com.backoffice.upjuyanolja.domain.room.entity;
 
 import lombok.Getter;
 
+/**
+ * 객실 상태 Enum
+ *
+ * @author JeongUijeong (jeong275117@gmail.com)
+ */
 @Getter
 public enum RoomStatus {
 
+    /**
+     * 객실 판매 중 상태
+     */
     SELLING("판매 중"),
+
+    /**
+     * 객실 판매 중지 상태
+     */
     STOP_SELLING("판매 중지");
 
+    /**
+     * 객실 상태 이름
+     */
     private final String name;
 
+    /**
+     * All Arguments Constructor
+     *
+     * @param name 객실 상태 이름
+     * @author JeongUijeong (jeong275117@gmail.com)
+     */
     RoomStatus(String name) {
         this.name = name;
     }
