16bf3bad1645101f4f380e513be6bc93bd817565
JeongUijeong <jeong275117@gmail.com>
2024-02-14 21:03:37 +0900

    #228 refactor: 카테고리 Custom Repository javadoc 작성

--- src/main/java/com/backoffice/upjuyanolja/domain/accommodation/repository/CategoryCustomRepository.java
+++ src/main/java/com/backoffice/upjuyanolja/domain/accommodation/repository/CategoryCustomRepository.java
@@ -10,5 +10,12 @@ import java.util.Optional;
  */
 public interface CategoryCustomRepository {
 
+    /**
+     * 카테고리 이름과 일치하는 하위 카테고리 Entity 조회 메서드
+     *
+     * @param name 조회하고자 하는 카테고리 이름
+     * @return 카테고리 이름과 일치하는 하위 카테고리 Optional 객체
+     * @author JeongUijeong (jeong275117@gmail.com)
+     */
     Optional<Category> findCategoryByName(String name);
 }
